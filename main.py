95b0fbe1aaaf8c7eb64f0c2e0f873e59b295a97d
suraj <s373211@oslomet.no>
2024-08-08 11:32:41 +0200

    added data ingestion and base model component

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 # from src.cnnClassifier import logger
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage01_data_ingestion import  DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
-
-
 
 try:
     logger.info(f">>-->>> stage {STAGE_NAME} started <---<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>--->>>> stage {STAGE_NAME} completed <<---<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"************")
+    logger.info(f">>>>>> stage{STAGE_NAME} started <<---<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<--<<\n\n x=====x")
 
 except Exception as e:
     logger.exception(e)
